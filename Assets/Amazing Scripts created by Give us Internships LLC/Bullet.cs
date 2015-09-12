2800281f6d2c3fc9aa3555989ae4995d46182d99
Ty Trusty <tytrusty@yahoo.com>
2015-09-11 22:14:55 -0500

    We got problems

---
+++ Assets/Amazing Scripts created by Give us Internships LLC/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+	public GameObject bullet;
+	public float projectileSpeed;
+
+	// Use this for initialization
+	void Start () {
+		projectileSpeed = 20f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			GameObject bigBoy = Instantiate (bullet);
+			bigBoy.transform.position = transform.position;
+			bigBoy.GetComponent<Rigidbody2D>().velocity = new Vector2(projectileSpeed,0); //Change y later dawg
+		}
+	}
+}
