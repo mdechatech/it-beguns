114655a3ddad7e737794a89e4635921351b652ce
Ty Trusty <tytrusty@yahoo.com>
2015-09-12 16:21:43 -0500

    Added in fadeIn script for text at the beginning of each scene

---
+++ Assets/NewSceneText.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewSceneText : MonoBehaviour {
+	CanvasGroup canvas;
+	bool fadeIn = false;
+	// Use this for initialization
+	void Start () {
+		canvas = GetComponent<CanvasGroup> ();
+		canvas.alpha = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(fadeIn == false)
+		{
+			canvas.alpha += .01f;
+		}
+		if(canvas.alpha == 1f)
+		{
+			fadeIn = true;
+		}
+
+		if (fadeIn == true && canvas.alpha > .75f) {
+			canvas.alpha -=.002f;
+		} 
+
+		if (fadeIn == true && canvas.alpha <= .75f) {
+			canvas.alpha -=.02f;
+		} 
+	}
+}
