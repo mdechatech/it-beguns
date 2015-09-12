f2c33ec48f4e98ea3faf4d992abb6790c0ff6004
Matthewit Dechatech <mdechatech@gmail.com>
2015-09-12 12:57:31 -0500

    Game manager

---
+++ Assets/Manager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class Manager : MonoBehaviour
+{
+    [SerializeField] public GameObject Player;
+    [SerializeField] public Boss Boss;
+
+    public UnityAction BossDied;
+
+    public int Wins;
+
+    public void Awake()
+    {
+        Wins = 0;
+    }
+
+    public void OnLevelWasLoaded(int level)
+    {
+        if (!Player.gameObject)
+        {
+            Player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        if (!Boss.gameObject)
+        {
+            Boss = GameObject.FindGameObjectWithTag("Boss")
+                .GetComponent<Boss>();
+            Boss.OnDeathEvent += OnBossDeath;
+        }
+    }
+
+	void Start () {
+	    DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Player == null || Boss == null) return;
+	}
+
+    public void OnBossDeath(object sender, EventArgs e)
+    {
+        ++Wins;
+    }
+}
