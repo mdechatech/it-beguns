e8ed7f5e0318e0be0396b79cedf8a8d43bc71394
Kevin Lam <kevin.lam72003@gmail.com>
2015-09-12 13:50:12 -0500

    spikes

--- Assets/PlayerMotion.cs
+++ Assets/PlayerMotion.cs
@@ -5,8 +5,16 @@ public class PlayerMotion : MonoBehaviour {
 
     public float moveSpeed;
     public float jumpSpeed;
-    public bool onFloor;
+    public float glideSpeed;
+
+    public int maxJumps;
+    public int jumpCount;
+    public bool gliding;
+
+    public bool dead;
+
     public Rigidbody2D rb;
+    public GameObject spawn;
 	// Use this for initialization
 	void Start () {
 	
@@ -18,18 +26,34 @@ public class PlayerMotion : MonoBehaviour {
         transform.Translate(horzMove, 0, 0);
 
 
-        if(onFloor && Input.GetKey(KeyCode.W))
+        if (jumpCount < maxJumps && Input.GetKeyDown(KeyCode.W))
         {
             rb.AddForce(Vector2.up * jumpSpeed);
-            onFloor = false;
+            jumpCount++;
         }
-            
-        
+        else if (jumpCount == maxJumps && Input.GetKeyDown(KeyCode.W))
+            gliding = true;
+
+        if (gliding && Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector2(0, glideSpeed);
+        }
+        else if (gliding && Input.GetKeyUp(KeyCode.W))
+            gliding = false;
+
+
+        if (dead)
+        {
+            transform.position = spawn.transform.position;
+            dead = false;
+            gliding = false;
+            jumpCount = 0;
+        }
 	}
 
     void OnCollisionEnter2D (Collision2D c)
     {
         if (c.gameObject.tag == "Floor" && c.contacts[0].normal.x == 0)
-            onFloor = true;
+            jumpCount = 0;
     }
 }
---
+++ Assets/SpikeScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        if(c.gameObject.tag == "Player")
+        {
+            PlayerMotion pm = c.gameObject.GetComponent<PlayerMotion>();
+            pm.dead = true;
+        }
+    }
+}
