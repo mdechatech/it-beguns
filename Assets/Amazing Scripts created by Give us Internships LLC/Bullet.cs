c7c0036f97464cf5b85382d8cff18d9bb94be8a7
Ty Trusty <tytrusty@yahoo.com>
2015-09-12 12:27:24 -0500

    Directional shooting

--- Assets/Amazing Scripts created by Give us Internships LLC/Bullet.cs
+++ Assets/Amazing Scripts created by Give us Internships LLC/Bullet.cs
@@ -4,23 +4,21 @@ using System.Collections;
 public class Bullet : MonoBehaviour {
 	public GameObject bullet;
 	public float projectileSpeed;
-	public Transform bigBoyTarget;
 	// Use this for initialization
 	void Start () {
-		projectileSpeed = 2000f;
+		projectileSpeed = 20f;
 	}
-	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown (0)) {
-			Vector2 target = Camera.main.ScreenToWorldPoint( new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y) );
+			Vector2 target = Camera.main.ScreenToWorldPoint( new Vector2(Input.mousePosition.x, Input.mousePosition.y) );
 			Vector2 myPos = new Vector2(transform.position.x,transform.position.y + 1);
 			Vector2 direction = target - myPos;
 			direction.Normalize();
-
-			GameObject bigBoy = (GameObject)Instantiate (bullet, myPos, Quaternion.identity);
+			Debug.Log (target);
+			GameObject bigBoy = (GameObject)Instantiate (bullet);
 			bigBoy.transform.position = transform.position;
-			bigBoy.GetComponent<Rigidbody2D>().velocity = direction*projectileSpeed; //Change y later dawg
+			bigBoy.GetComponent<Rigidbody2D>().velocity = direction*projectileSpeed; 
 			
 		}
 
