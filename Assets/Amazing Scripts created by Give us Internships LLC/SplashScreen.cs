d31100aa6cab6188162193a6a633434de3f4fbfc
Ty Trusty <tytrusty@yahoo.com>
2015-09-11 21:28:37 -0500

    Added a really ugly flash screen

---
+++ Assets/Amazing Scripts created by Give us Internships LLC/SplashScreen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour {
+	CanvasGroup canvas;
+
+	// Use this for initialization
+	void Start () {
+		canvas = GetComponent<CanvasGroup> ();
+		canvas.alpha = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		canvas.alpha -= .1f;
+	}
+}
