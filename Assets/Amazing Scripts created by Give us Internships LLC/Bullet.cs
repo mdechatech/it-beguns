d6d5f874acf66ca4edc4b93c544cd6eb185428a9
Ty Trusty <tytrusty@yahoo.com>
2015-09-12 12:45:22 -0500

    Trail effects and junk like that

--- Assets/Amazing Scripts created by Give us Internships LLC/Bullet.cs
+++ Assets/Amazing Scripts created by Give us Internships LLC/Bullet.cs
@@ -4,22 +4,27 @@ using System.Collections;
 public class Bullet : MonoBehaviour {
 	public GameObject bullet;
 	public float projectileSpeed;
+	public float fireRate = -.2f;
 	// Use this for initialization
 	void Start () {
-		projectileSpeed = 20f;
+		projectileSpeed = 30f;
+		fireRate = Time.time;
 	}
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
-			Vector2 target = Camera.main.ScreenToWorldPoint( new Vector2(Input.mousePosition.x, Input.mousePosition.y) );
-			Vector2 myPos = new Vector2(transform.position.x,transform.position.y + 1);
-			Vector2 direction = target - myPos;
-			direction.Normalize();
-			Debug.Log (target);
-			GameObject bigBoy = (GameObject)Instantiate (bullet);
-			bigBoy.transform.position = transform.position;
-			bigBoy.GetComponent<Rigidbody2D>().velocity = direction*projectileSpeed; 
-			
+
+		if(Time.time>=fireRate){
+			if (Input.GetMouseButtonDown (0)) {
+				fireRate = Time.time + .2f;
+				Vector2 target = Camera.main.ScreenToWorldPoint( new Vector2(Input.mousePosition.x, Input.mousePosition.y) );
+				Vector2 myPos = new Vector2(transform.position.x,transform.position.y + 1);
+				Vector2 direction = target - myPos;
+				direction.Normalize();
+				Debug.Log (target);
+				GameObject bigBoy = (GameObject)Instantiate (bullet);
+				bigBoy.transform.position = transform.position;
+				bigBoy.GetComponent<Rigidbody2D>().velocity = direction*projectileSpeed; 
+			}	
 		}
 
 	}
