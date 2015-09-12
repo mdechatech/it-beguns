8d67295d10ef38534f3ce7f80aa437c9fb1d1cb6
Ty Trusty <tytrusty@yahoo.com>
2015-09-12 11:20:32 -0500

    Made BadGuyDestroyBullet scripts that kills the bullets on collision.

---
+++ Assets/Amazing Scripts created by Give us Internships LLC/BadGuyDestroyBullet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BadGuyDestroyBullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter2D(Collision2D Bullet){
+		if (Bullet.gameObject.tag == "Bullet") {
+			Destroy(Bullet.gameObject);
+			Debug.Log("NIce shot dawg");
+			
+			//Call a method in the object to remove boss health
+			//Then do an if statement asking if(blahblahblah.getHealth() <= 0) then Destroy(badGuy.gameObject) I THINK????
+			//THen when bad guy is destroyed, set a boolean value for something like bool bossDead, and then check if boss is dead in update method then move to next stage
+		}
+	}
+}
--- Assets/Amazing Scripts created by Give us Internships LLC/Bullet.cs
+++ Assets/Amazing Scripts created by Give us Internships LLC/Bullet.cs
@@ -5,7 +5,6 @@ public class Bullet : MonoBehaviour {
 	public GameObject bullet;
 	public float projectileSpeed;
 	public Transform bigBoyTarget;
-
 	// Use this for initialization
 	void Start () {
 		projectileSpeed = 2000f;
@@ -17,15 +16,10 @@ public class Bullet : MonoBehaviour {
 			GameObject bigBoy = Instantiate (bullet);
 			bigBoy.transform.position = transform.position;
 			bigBoy.GetComponent<Rigidbody2D>().velocity = (bigBoyTarget.position - transform.position).normalized*projectileSpeed; //Change y later dawg
+			
+		}
 
-		}
 	}
-	void onHitBadGuy(Collider badGuy){
-		if (badGuy.tag == "Boss") {
-			//Instantiate new boss object
-			//Call a method in the object to remove boss health
-			//Then do an if statement asking if(blahblahblah.getHealth() <= 0) then Destroy(badGuy.gameObject) I THINK????
-		}
-	}
+	
 
 }
