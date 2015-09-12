bbd16d95dd6b13ccebf2b84e271d24112b643d93
Matthewit Dechatech <mdechatech@gmail.com>
2015-09-12 13:52:53 -0500

    Test level

--- Assets/Boss.cs
+++ Assets/Boss.cs
@@ -11,13 +11,24 @@ public class Boss : MonoBehaviour
     private GameObject _player;
 
     [SerializeField]
-    public int Health;
+    public int Health = 20;
 
     [SerializeField] 
     public UnityEvent OnDeath;
 
     [HideInInspector]
     public event EventHandler<EventArgs> OnDeathEvent;
+
+    [SerializeField] public UnityEvent OnHit;
+
+    public event EventHandler<EventArgs> OnHitEvent;
+
+    private SpriteRenderer _spriteRenderer;
+
+    public void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     public void OnTriggerEnter2D(Collider2D collider2D)
     {
@@ -26,10 +37,24 @@ public class Boss : MonoBehaviour
         Destroy(collider2D.gameObject);
         --Health;
 
+        OnHit.Invoke();
+        if (OnHitEvent != null)
+        {
+            OnHitEvent(this, EventArgs.Empty);
+        }
+
+        _spriteRenderer.color = new Color(1.0f, 0.75f, 0.75f, 1.0f);
+
         if (Health <= 0)
         {
+            Health = 0;
             OnDeath.Invoke();
             if (OnDeathEvent != null) OnDeathEvent(this, EventArgs.Empty);
         }
+    }
+
+    public void Update()
+    {
+        _spriteRenderer.color = Color.Lerp(_spriteRenderer.color, Color.white, Time.deltaTime);
     }
 }
---
+++ Assets/Shotgun.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shotgun : MonoBehaviour
+{
+    [SerializeField] public GameObject Bullet;
+    [SerializeField] public GameObject Target;
+
+    [SerializeField] public float FireRate;
+    [SerializeField] public int FireAmount;
+    [SerializeField] public float FireSpread;
+    [SerializeField] public float FireSpeed;
+
+    private float _fireTimer;
+    [HideInInspector] public bool CanFire;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _fireTimer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	public void Update ()
+	{
+	    _fireTimer += Time.deltaTime;
+	    if (_fireTimer >= FireRate)
+	    {
+	        CanFire = true;
+	    }
+	}
+
+    public void Fire()
+    {
+        if (!CanFire) return;
+        _fireTimer = 0.0f;
+
+        var targetVector = Target.transform.position - transform.position;
+        var targetAngle = Mathf.Atan2(targetVector.y, targetVector.x)*Mathf.Rad2Deg - FireSpread/2;
+        for (var i = 0; i < FireAmount; i++)
+        {
+            FireBullet(targetAngle * Mathf.Deg2Rad);
+            targetAngle += FireSpread/FireAmount;
+        }
+    }
+
+    private void FireBullet(float angle)
+    {
+        var bullet = Instantiate(Bullet);
+        bullet.transform.position = transform.position;
+        bullet.tag = "EnemyBullet";
+
+        var rigidbody2D = bullet.GetComponent<Rigidbody2D>();
+        rigidbody2D.velocity = (new Vector2(Mathf.Cos(angle), Mathf.Sin(angle))) * FireSpeed;
+    }
+}
