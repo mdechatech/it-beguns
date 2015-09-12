4a0974a8a5a1ade1f5f4073cb0e2a58609f7a3e3
Matthewit Dechatech <mdechatech@gmail.com>
2015-09-12 12:28:30 -0500

    Empty boss

---
+++ Assets/Boss.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class Boss : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _player;
+
+    [SerializeField]
+    public int Health;
+
+    [SerializeField] 
+    public UnityEvent OnDeath;
+
+    public void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        var bullet = collider2D.GetComponent<Bullet>();
+        if (bullet == null) return;
+
+        Destroy(bullet.gameObject);
+        --Health;
+
+        if (Health <= 0)
+        {
+            OnDeath.Invoke();
+        }
+    }
+}
